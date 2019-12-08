117d9e9c664b4fed0f849f9ffc62a7edd6b6d36e
Ismael Azaran <eazaran@gmail.com>
2019-12-09 00:36:33 +0300

    Create new CSRF if doesn't exist

--- public/index.php
+++ public/index.php
@@ -1,10 +1,13 @@
 <?php
+
 /**
  * Random token to use as a CSRF with an expiration time
  */
 session_start();
-$_SESSION['token'] = substr(base_convert(sha1(uniqid(mt_rand())), 16, 36), 0, 32);
-$_SESSION['token-expire'] = time() + 3600;
+if (!isset($_SESSION['token'])) {
+    $_SESSION['token'] = substr(base_convert(sha1(uniqid(mt_rand())), 16, 36), 0, 32);
+    $_SESSION['token-expire'] = time() + 3600;
+}
 
 /**
  * Configure autoloading
