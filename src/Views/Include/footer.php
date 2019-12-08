a4bef9cf32ec6af1c9b6df0ec412216a21194014
Ismael Azaran <eazaran@gmail.com>
2019-12-09 00:37:11 +0300

    Use main jQuery CDN to use extra features in Bootstrap

--- src/Views/Include/footer.php
+++ src/Views/Include/footer.php
@@ -2,11 +2,14 @@
     <small>Made with ❤️ in GiliApps . Powered by <abbr title="Pure PHP MVC">PPMVC</abbr> . <a href="https://github.com/iazaran/php-mvc" target="_blank" class="text-dark border border-dark rounded-pill px-1">GitHub</a></small>
 </footer>
 <!-- Bootstrap scripts -->
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 <!-- Custom script -->
+<script>
+    var api_address = '<?= URL_ROOT; ?>';
+</script>
 <script src="/js/main.js"></script>
 </body>
 
