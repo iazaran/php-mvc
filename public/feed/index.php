7e9ac0f9515173f0f64f78a56cfd72038366a634
Ismael Azaran <eazaran@gmail.com>
2020-05-23 13:29:33 +0300

    RSS updated with a camelCase var

--- public/feed/index.php
+++ public/feed/index.php
@@ -3,32 +3,32 @@ header("Content-Type: application/rss+xml; charset=UTF-8");
 
 use App\Database;
 
-$rssfeed = '<?xml version="1.0" encoding="utf-8"?>';
-$rssfeed .= '<rss xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0">';
-$rssfeed .= '<channel>';
-$rssfeed .= '<title>' . TITLE . '</title>';
-$rssfeed .= '<link>' . URL_ROOT . '/blog/</link>';
-$rssfeed .= '<language>en_US</language>';
-$rssfeed .= '<generator>' . URL_ROOT . '</generator>';
-$rssfeed .= '<description>' . SUBTITLE . '</description>';
-$rssfeed .= '<copyright>Copyright © ' . date("Y") . ' ' . TITLE . '</copyright>';
+$rssFeed = '<?xml version="1.0" encoding="utf-8"?>';
+$rssFeed .= '<rss xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0">';
+$rssFeed .= '<channel>';
+$rssFeed .= '<title>' . TITLE . '</title>';
+$rssFeed .= '<link>' . URL_ROOT . '/blog/</link>';
+$rssFeed .= '<language>en_US</language>';
+$rssFeed .= '<generator>' . URL_ROOT . '</generator>';
+$rssFeed .= '<description>' . SUBTITLE . '</description>';
+$rssFeed .= '<copyright>Copyright © ' . date("Y") . ' ' . TITLE . '</copyright>';
 
 Database::query("SELECT * FROM posts ORDER BY id DESC LIMIT :count");
 Database::bind(':count', RSS_COUNTS);
 $posts = Database::fetchAll();
 foreach ($posts as $post) {
-    $rssfeed .= '<item>';
-    $rssfeed .= '<title>' . rssXml($post['title']) . '</title>';
-    $rssfeed .= '<category>' . rssXml($post['category']) . '</category>';
-    $rssfeed .= '<description>' . rssXml($post['subtitle']) . '</description>';
-    $rssfeed .= '<link>' . URL_ROOT . '/blog/' . $post['slug'] . '</link>';
-    $rssfeed .= '<pubDate>' . $post['updated_at'] . '</pubDate>';
-    $rssfeed .= '<dc:creator>' . TITLE . '</dc:creator>';
-    $rssfeed .= '</item>';
+    $rssFeed .= '<item>';
+    $rssFeed .= '<title>' . rssXml($post['title']) . '</title>';
+    $rssFeed .= '<category>' . rssXml($post['category']) . '</category>';
+    $rssFeed .= '<description>' . rssXml($post['subtitle']) . '</description>';
+    $rssFeed .= '<link>' . URL_ROOT . '/blog/' . $post['slug'] . '</link>';
+    $rssFeed .= '<pubDate>' . $post['updated_at'] . '</pubDate>';
+    $rssFeed .= '<dc:creator>' . TITLE . '</dc:creator>';
+    $rssFeed .= '</item>';
 }
 
-$rssfeed .= '</channel>';
-$rssfeed .= '</rss>';
+$rssFeed .= '</channel>';
+$rssFeed .= '</rss>';
 
-file_put_contents("feed/rss.xml", $rssfeed);
+file_put_contents("feed/rss.xml", $rssFeed);
 header("refresh:0;url=rss.xml");
